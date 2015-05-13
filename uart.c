5d2314aef3a0f816c8963995199a9e7784967c6c
MarinOlivier <olivier.marin.seal2@gmail.com>
2015-05-13 15:13:04 +0200

    uart.c/uart.h

--- uart.c
+++ uart.c
@@ -23,6 +23,38 @@ void initUART(void){
     P3SEL |= BIT4 + BIT5;               // P3.4,5 = USART0 TXD/RXD
     P3DIR |= BIT4;
     
+    _EINT();
 
   return;
+}
+
+
+void UARTprintTerm(const char * str)
+{
+  int i;
+
+    while(*str != '\0' )
+    {
+   	 while(!(IFG1& UTXIFG0));  // USART0 TX buffer pret?
+   	 TXBUF0 = *str++;
+    }
+
+    for(i = 0; i < 10000; i = i+1)
+    {
+    }
+}
+
+void UARTmsgAccueil(void)
+{
+    UARTprintTerm("\n\r===========================================================================\n");
+    UARTprintTerm("\rBievenue dans le menu de gestion de la communication avec le microcontrôleur\n");
+    UARTprintTerm("\r===========================================================================\n");
+    UARTprintTerm("\r===  Menu  ===\n");
+    UARTprintTerm("\rTouche 't' : Affichage de la température\n");
+    UARTprintTerm("\rTouche 's' : Arrêt de l'affichage de la température\n");
+    UARTprintTerm("\rTouche 'c' : Saisi de la consigne de température\n");
+    UARTprintTerm("\rTouche 'p' : Affichage de la température de consigne\n");
+    UARTprintTerm("\rTouche 'l' : Démarrer la régulation PWM\n");
+    UARTprintTerm("\rTouche 'm' : Arrêter la régulation PWM\n");
+    UARTprintTerm("\n\rVotre choix ? ");
 }
--- uart.h
+++ uart.h
@@ -1,6 +1,8 @@
 #ifndef __UART__
 #define __UART__
 
-
+void initUART(void);
+void UARTprintTerm(const char * str);
+void UARTmsgAccueil(void);
 
 #endif
